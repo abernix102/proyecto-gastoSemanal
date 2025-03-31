ea90a5b80f453cbd89cb2dc3ead75dd171dec1d2
Nahuel <nahuelo841@gmail.com>
2025-03-31 18:05:35 -0300

    classPresupuesto

--- js/app.js
+++ js/app.js
@@ -10,19 +10,28 @@ function eventListener(){
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 
+dialog.style.position = "fixed";
+dialog.style.zIndex = "9999";
 
 //classes
 class Presupuesto {
-    constructor(Presupuesto){
-        this.presupuesto = Number(Presupuesto);
-        this.restante = Number(this.restante);
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
         this.gastos = []
     }
 }
 
 class UI{
+    insertarPresupuesto(cantidad) {
+        const {presupuesto, restante} = cantidad;
+        document.querySelector("#total").textContent = presupuesto;
+        document.querySelector("#restante").textContent = restante;
 
+    }
 }
+const ui = new UI();
+let presupuesto;
 
 //functiones
 function preguntarPresupuesto(){
@@ -30,8 +39,15 @@ function preguntarPresupuesto(){
 
     closeButton.disabled = true
 
-    input.addEventListener('click', () => {
-        const inputValue = Number(input.value.trim())
-        closeButton.disabled = inputValue <= 0 || isNaN(inputValue);  
+    input.addEventListener('input', () => {
+        const inputValue = parseFloat(input.value)
+        closeButton.disabled = inputValue <= 0 || isNaN(inputValue);
+    })
+
+    closeButton.addEventListener("click", ()=> {
+        presupuesto = new Presupuesto(parseFloat(input.value))
+        ui.insertarPresupuesto(presupuesto)
+        dialog.close();
+        dialog.style.display ="none";
     })
 }
