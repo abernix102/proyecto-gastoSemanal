ad814cf1b55da89c089203c547d6541b042d18ea
Nahuel <nahuelo841@gmail.com>
2025-04-03 14:59:57 -0300

    functiones

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //variable y selectores
-const formulario = document.querySelector("#agregar-gato");
+const formulario = document.querySelector("#agregar-gasto");
 const gastoListado = document.querySelector("#gastos ul");
 const dialog = document.getElementById("myDialog");
 const closeButton = dialog.querySelector("button");
@@ -8,6 +8,7 @@ const input = dialog.querySelector("input")
 eventListener();
 function eventListener(){
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    formulario.addEventListener('submit', guardarDatos);
 }
 
 dialog.style.position = "fixed";
@@ -29,7 +30,22 @@ class UI{
         document.querySelector("#restante").textContent = restante;
 
     }
+    imprimitAlerta(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        if(tipo === "error"){
+            divMensaje.classList.add('alert-danger')
+        }else{
+            divMensaje.classList.add('alert-success')
+        }
+        divMensaje.textContent = mensaje;
+        document.querySelector('.primario').insertBefore(divMensaje, formulario)
+        setTimeout(() => {
+            divMensaje.remove()
+        }, 3000);
+    }
 }
+
 const ui = new UI();
 let presupuesto;
 
@@ -40,14 +56,28 @@ function preguntarPresupuesto(){
     closeButton.disabled = true
 
     input.addEventListener('input', () => {
-        const inputValue = parseFloat(input.value)
+        const inputValue = Number(input.value)
         closeButton.disabled = inputValue <= 0 || isNaN(inputValue);
     })
 
     closeButton.addEventListener("click", ()=> {
-        presupuesto = new Presupuesto(parseFloat(input.value))
-        ui.insertarPresupuesto(presupuesto)
         dialog.close();
         dialog.style.display ="none";
+        presupuesto = new Presupuesto(Number(input.value))
+        ui.insertarPresupuesto(presupuesto)
     })
+}
+
+function guardarDatos(e){
+    e.preventDefault();
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    if(nombre === "" || cantidad === ""){
+        ui.imprimitAlerta("ambos campos son obligatorios", "error")
+        return
+    }else if(cantidad <= 0 || isNaN(cantidad)){
+        ui.imprimitAlerta("cantidad valida", "error");
+        return
+    }
 }
