cc660350c769d010c3acaea93f852237ad3b0d03
Nahuel <nahuelo841@gmail.com>
2025-04-05 01:03:09 -0300

    agregarGastoListado

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ const formulario = document.querySelector("#agregar-gasto");
 const gastoListado = document.querySelector("#gastos ul");
 const dialog = document.getElementById("myDialog");
 const closeButton = dialog.querySelector("button");
-const input = dialog.querySelector("input")
+const input = dialog.querySelector("input");
 //eventos
 eventListener();
-function eventListener(){
-    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
-    formulario.addEventListener('submit', guardarDatos);
+function eventListener() {
+  document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+  formulario.addEventListener("submit", guardarDatos);
 }
 
 dialog.style.position = "fixed";
@@ -16,68 +16,99 @@ dialog.style.zIndex = "9999";
 
 //classes
 class Presupuesto {
-    constructor(presupuesto){
-        this.presupuesto = Number(presupuesto);
-        this.restante = Number(presupuesto);
-        this.gastos = []
-    }
+  constructor(presupuesto) {
+    this.presupuesto = Number(presupuesto);
+    this.restante = Number(presupuesto);
+    this.gastos = [];
+  }
+  nuevoGasto(gasto) {
+    this.gastos = [...this.gastos, gasto];
+  }
 }
 
-class UI{
-    insertarPresupuesto(cantidad) {
-        const {presupuesto, restante} = cantidad;
-        document.querySelector("#total").textContent = presupuesto;
-        document.querySelector("#restante").textContent = restante;
+class UI {
+  insertarPresupuesto(cantidad) {
+    const { presupuesto, restante } = cantidad;
+    document.querySelector("#total").textContent = presupuesto;
+    document.querySelector("#restante").textContent = restante;
+  }
+  imprimitAlerta(mensaje, tipo) {
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert");
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+    divMensaje.textContent = mensaje;
+    document.querySelector(".primario").insertBefore(divMensaje, formulario);
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+  agregarGastoListado(gastos) {
+    gastos.forEach(element => {
+      const { cantidad, nombre, id } = element;
+      const nuevoGasto = document.createElement("li");
+      nuevoGasto.className =
+        "list-group-item d-flex justify-content-between align-items-center";
+      nuevoGasto.dataset.id = id;
 
-    }
-    imprimitAlerta(mensaje, tipo){
-        const divMensaje = document.createElement('div');
-        divMensaje.classList.add('text-center', 'alert');
-        if(tipo === "error"){
-            divMensaje.classList.add('alert-danger')
-        }else{
-            divMensaje.classList.add('alert-success')
-        }
-        divMensaje.textContent = mensaje;
-        document.querySelector('.primario').insertBefore(divMensaje, formulario)
-        setTimeout(() => {
-            divMensaje.remove()
-        }, 3000);
-    }
+      const nombreTexto = document.createTextNode(nombre);
+      //crea el span para el badge
+      const span = document.createElement("span");
+      span.classList = "badge badge-primary badge-pill";
+      span.textContent = cantidad;
+      nuevoGasto.appendChild(nombreTexto);
+      nuevoGasto.appendChild(span);
+      //btn para borrar
+      const btnBorrar = document.createElement("button");
+      btnBorrar.classList.add("btn", "btn-danger", "borrar-gasto");
+      nuevoGasto.appendChild(btnBorrar);
+
+      gastoListado.appendChild(nuevoGasto);
+    });
+  }
 }
 
 const ui = new UI();
 let presupuesto;
 
 //functiones
-function preguntarPresupuesto(){
-    dialog.showModal()
+function preguntarPresupuesto() {
+  dialog.showModal();
 
-    closeButton.disabled = true
+  closeButton.disabled = true;
 
-    input.addEventListener('input', () => {
-        const inputValue = Number(input.value)
-        closeButton.disabled = inputValue <= 0 || isNaN(inputValue);
-    })
+  input.addEventListener("input", () => {
+    const inputValue = Number(input.value);
+    closeButton.disabled = inputValue <= 0 || isNaN(inputValue);
+  });
 
-    closeButton.addEventListener("click", ()=> {
-        dialog.close();
-        dialog.style.display ="none";
-        presupuesto = new Presupuesto(Number(input.value))
-        ui.insertarPresupuesto(presupuesto)
-    })
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+    dialog.style.display = "none";
+    presupuesto = new Presupuesto(Number(input.value));
+    ui.insertarPresupuesto(presupuesto);
+  });
 }
 
-function guardarDatos(e){
-    e.preventDefault();
-    const nombre = document.querySelector('#gasto').value;
-    const cantidad = document.querySelector('#cantidad').value;
+function guardarDatos(e) {
+  e.preventDefault();
+  const nombre = document.querySelector("#gasto").value;
+  const cantidad = document.querySelector("#cantidad").value;
 
-    if(nombre === "" || cantidad === ""){
-        ui.imprimitAlerta("ambos campos son obligatorios", "error")
-        return
-    }else if(cantidad <= 0 || isNaN(cantidad)){
-        ui.imprimitAlerta("cantidad valida", "error");
-        return
-    }
+  if (nombre === "" || cantidad === "") {
+    ui.imprimitAlerta("ambos campos son obligatorios", "error");
+    return;
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    ui.imprimitAlerta("cantidad valida", "error");
+    return;
+  }
+
+  const gasto = { nombre, cantidad, id: Date.now() };
+  presupuesto.nuevoGasto(gasto);
+  ui.imprimitAlerta("Gasto agregado con exito");
+  ui.agregarGastoListado(presupuesto.gastos); // ← esto sí le pasa el array completo
+  formulario.reset();
 }
